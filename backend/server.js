c9d91ae2cd255557456901cda5de440fff0e3126
Pratik Lamichhane <lcpratik75@gmail.com>
2026-08-12 19:34:58 -0500

    alphaVantage.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,23 @@
 import "dotenv/config";
+console.log("API KEY LOADED:", process.env.ALPHA_VANTAGE_API_KEY);
+import { getDailyPrices } from "./services/alphaVantage.js";
 import express from "express";
 import cors from "cors";
+
 
 const app = express(); // express is a function that returns an object that represents the app. This object has methods for routing HTTP requests, configuring middleware, rendering HTML views, registering a template engine, and modifying application settings.
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/api/health", (req, res) => {
-  res.json({ message: "Okay!" });
+app.get("/api/test-prices/:ticker", async (req, res) => {
+  try {
+    const prices = await getDailyPrices(req.params.ticker);
+    res.json(prices);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Failed to fetch daily prices" });
+  }
 });
 
 app.listen(4000, () => {
--- backend/services/alphaVantage.js
+++ backend/services/alphaVantage.js
@@ -6,19 +6,21 @@ import axios from "axios";
             params: {
                 function: "TIME_SERIES_DAILY",
                 symbol: ticker,
-                outputsize: "full",
+                outputsize: "compact",
                 apikey: process.env.ALPHA_VANTAGE_API_KEY
             }
         });
+        console.log(response.data);
         const series = response.data["Time Series (Daily)"];
         if(!series) {
             throw new Error("Invalid response from Alpha Vantage API");
         }
         const prices = Object.entries(series).map(([date, data]) => ({
             date,
-            close: parseFloat(data["4. close"]),
+            close: parseFloat(data["4. close"]), //4. means 
         }));
-        prices.sort((a, b) => new Date(a.date) - new Date(b.date));
+        prices.sort((a, b) => new Date(a.date) - new Date(b.date)); // Sort prices by date in ascending order. negative value means a comes before b, 
+        // positive value means b comes before a, and 0 means they are equal. So, if we get negative value that means it is already sorted in ascending order. If we get positive value that means it is sorted in descending order. If we get 0 that means they are equal and we can keep them in the same order.
         return prices;
     } catch (error) {
         console.error(error);
