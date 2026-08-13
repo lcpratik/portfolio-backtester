48ab1e9c050d87749a192cc4fc8545db1e594e08
Pratik Lamichhane <lcpratik75@gmail.com>
2026-08-13 18:01:48 -0500

    calculation.js

--- backend/utils/calculation.js
+++ backend/utils/calculation.js
@@ -14,11 +14,29 @@ function alignByDate(pricesByTicker) {
     for (const t of tickers) {
         closeByTickerAndDate[t] = {};   
 
-        for (const p of pricesByTicker[t])  // price by ticker only has date and closee
+        for (const p of pricesByTicker[t])  
         {
             closeByTickerAndDate[t][p.date] = p.close;  
         }
     }
 
   return { commonDates, closeByTickerAndDate };
+}
+
+function simulateBuyAndHold(pricesByTicker, allocations, startingAmount, startDate, endDate) {
+  const { commonDates, closeByTickerAndDate } = alignByDate(pricesByTicker);
+
+  const datesInRange = commonDates.filter(
+  (date) => date >= startDate && date <= endDate
+    );
+
+  const firstdate = datesInRange[0];
+
+  // step 3: for each ticker in `allocations`, compute how many shares were bought
+  //   shares[ticker] = (startingAmount * allocations[ticker]) / closeByTickerAndDate[ticker][firstDate]
+
+  // step 4: for each date in the filtered range, compute total portfolio value that day
+  //   value = sum over all tickers of (shares[ticker] * closeByTickerAndDate[ticker][date])
+
+  // return an array of { date, value } objects
 }
