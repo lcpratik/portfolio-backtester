5d4cd212ca3f55668b4cd6c02ddf0d7fe6a6bdee
Pratik Lamichhane <lcpratik75@gmail.com>
2026-08-13 14:46:03 -0500

    calculation.js

--- backend/utils/calculation.js
+++ backend/utils/calculation.js
@@ -1,15 +1,23 @@
 function alignByDate(pricesByTicker) {
-  const tickers = Object.keys(pricesByTicker); // tickers means symbols
+  const tickers = Object.keys(pricesByTicker); 
 
-  // Build a Set of dates for each ticker, so we can quickly check "does this ticker have this date?"
   const dateSets = tickers.map(
     (t) => new Set(pricesByTicker[t].map((p) => p.date))
   );
 
-  // Start from the first ticker's dates, keep only dates that exist in EVERY ticker's Set 
   const commonDates = pricesByTicker[tickers[0]]
     .map((p) => p.date)
     .filter((date) => dateSets.every((set) => set.has(date)));
 
-  return commonDates;
+    const closeByTickerAndDate = {};  
+
+for (const t of tickers) {
+  closeByTickerAndDate[t] = {};   
+
+  for (const p of pricesByTicker[t]) {
+    closeByTickerAndDate[t][p.date] = p.close;  
+  }
+}
+
+  return { commonDates, closeByTickerAndDate };
 }
