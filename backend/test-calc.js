54360fd709a731c6634bca7e0c610db0259f0573
Pratik Lamichhane <lcpratik75@gmail.com>
2026-08-13 19:27:20 -0500

    calculation.js

--- backend/test-calc.js
+++ backend/test-calc.js
@@ -1,8 +1,11 @@
-import { simulateBuyAndHold, computeStats } from "./utils/calculation.js";
+import { simulateBuyAndHold, computeStats } from "./utils/calculations.js";
+
 const pricesByTicker = {
   VOO: [
     { date: "2023-01-01", close: 100 },
-    { date: "2024-01-01", close: 120 },
+    { date: "2023-06-01", close: 120 },
+    { date: "2023-09-01", close: 90 },
+    { date: "2024-01-01", close: 130 },
   ],
 };
 
