efb39b32b686f473f58d8ed1abb9a11199588fe9
Pratik Lamichhane <lcpratik75@gmail.com>
2026-08-15 16:52:23 -0500

    frontend

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ import express from "express";
 import cors from "cors";
 
 
-const app = express(); // express is a function that returns an object that represents the app. This object has methods for routing HTTP requests, configuring middleware, rendering HTML views, registering a template engine, and modifying application settings.
+const app = express(); // express is a function that returns an object that represents the app. This object has methods for routing HTTP requests, configuring middleware, rendering HTML views, 
+// registering a template engine, and modifying application settings.
 
 app.use(express.json());
 app.use(cors());
