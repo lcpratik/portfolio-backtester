1203b2fb2df01f19afdf7f4e31759d7323a88c30
Pratik Lamichhane <lcpratik75@gmail.com>
2026-08-15 19:57:24 -0500

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
 import { useState } from 'react'
 
 function App() {
-  const [startingAmount, setStartingAmount] = useState(0);
+  const [ticker, setTicker] = useState({ symbol: "VOO", allocation: 40 });
 
   return (
     <div>
-      <p>Starting amount: {startingAmount}</p>
+      <p>{ticker.symbol}: {ticker.allocation}%</p>
+
+      <input
+        type="text"
+        value={ticker.symbol}
+        onChange={(e) => setTicker({ symbol: e.target.value, allocation: ticker.allocation })}
+      />
+
       <input
         type="number"
-        value={startingAmount}
-        onChange={(e) => setStartingAmount(e.target.value)}
+        value={ticker.allocation}
+        onChange={(e) => setTicker({ symbol: ticker.symbol, allocation: e.target.value })}
       />
     </div>
   )
